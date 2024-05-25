14bbec8a6e4fa37fb7087344d20fc1b5d13b8df6
MartinsALaura <lauraalvesmartins04@gmail.com>
2024-05-25 16:24:36 -0300

    Create class Produto

---
+++ src/Produto.java
@@ -0,0 +1,44 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private Data validade;
+    
+    public Produto(String nome, double preco, Data validade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.validade = validade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Data getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Data validade) {
+        this.validade = validade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto [nome=" + nome + ", preco=" + preco + ", validade=" + validade + "]";
+    }
+
+    public boolean verificarodutoVencido(Data data){
+        return data == this.validade;
+    }
+}
